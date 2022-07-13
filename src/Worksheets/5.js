e383a8fc6c81c52113a1c43dabb8aac3d678aed7
J-dance <dance.jojo@gmail.com>
2022-07-13 12:11:10 +0100

    completed exrcise sheet 5

--- src/Worksheets/5.js
+++ src/Worksheets/5.js
@@ -4,7 +4,9 @@ import ExerciseTextBlock from "../components/ExerciseTextBlock/ExerciseTextBlock
 import MyFigure from "../components/MyFigure/MyFigure";
 import "./worksheets.css";
 import img1 from '../assets/sheet5/exercise1.png';
-import img2 from '../assets/sheet5/exercise2.png'
+import img2 from '../assets/sheet5/exercise2.png';
+import img3 from '../assets/sheet5/exercise3.png';
+import img4 from '../assets/sheet5/exercise3-footer.png';
 
 function Worksheet5() {
 
@@ -46,6 +48,8 @@ function Worksheet5() {
       />
 
       <p><span className="bold">Task</span>: In the Codepen below, edit the code and use the grid method to arrange the coloured div elements to match the image below.</p>
+
+      <p className="italic">Note: you only need to edit the CSS file. The div elements will expand in size to fill a grid area.</p>
       
       <MyFigure
         imgSrc={img2} 
@@ -62,8 +66,37 @@ function Worksheet5() {
 
 
       {/* 3. Build to match profile site with chosen methods */}
+      <ExerciseTextBlock
+        title="3. Using Flexbox and grids"
+        text="In this exercise you are going to use what you learnt to layout a web page."
+      />
 
-      
+      <p><span className="bold">Task</span>: In the Codepen below, edit the code and use what you have learnt to arrange the web page to match the image below.</p>
+
+      <p className="italic">Note: there is no single correct way of completing this task so long as it matches the image below</p>
+
+      <MyFigure
+        imgSrc={img3} 
+        width="70%"
+        altText="exercise 3 showing web page layout" 
+        caption="Fig 3: Web page layout using Flexbox and grid"  
+      />
+
+      <p><span className="bold">Extra task</span>: In the same Codepen editor below, add a footer to the wab page as shown in the image below.</p>
+
+      <MyFigure
+        imgSrc={img4} 
+        width="80%"
+        altText="exercise 3 extra exercise showing a footer" 
+        caption="Fig 4: Footer for web page"  
+      />
+
+      <iframe height="500" width="100%" scrolling="no" title="Sheet 5 exercise 3 start" src={`https://codepen.io/futurecodersse/embed/YzaGjvp?default-tab=html%2Cresult&editable=true`} frameBorder="no" loading="lazy" allowtransparency="true" allowFullScreen={true}>
+        See the Pen <a href="https://codepen.io/futurecodersse/pen/YzaGjvp">
+        Sheet 5 exercise 3 start</a> by futureCoders (<a href="https://codepen.io/futurecodersse">@futurecodersse</a>)
+        on <a href="https://codepen.io">CodePen</a>.
+      </iframe>
+     
     </PageTemplate>
   )
 };
